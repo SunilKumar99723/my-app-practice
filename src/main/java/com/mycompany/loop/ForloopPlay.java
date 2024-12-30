58192e93b884f13f83524718f63553e7cd62d5ea
Sunil Kumar <60743582+SunilKumar99723@users.noreply.github.com>
2024-12-30 13:31:08 +0530

    First commit

--- src/main/java/com/mycompany/loop/ForloopPlay.java
+++ src/main/java/com/mycompany/loop/ForloopPlay.java
@@ -5,6 +5,7 @@ public class ForloopPlay {
 	public static void main(String[] args) {
 		
 		for(int i=0;i<4;i++)
+
 		{
 			for(int j=i+1;j<5;j++) {
 				System.out.print("i value "+i+ " j value "+j);
